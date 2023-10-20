7ffe63b9be85a5b037b8d0e2585b09a0761358c5
mike <maicolflorez1305@gmail.com>
2023-10-20 17:51:17 -0500

    Avance Maicol Florez

--- app/src/main/java/com/mflorezddelgado/t_mate/Register_Activity.java
+++ app/src/main/java/com/mflorezddelgado/t_mate/Register_Activity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 public class Register_Activity extends AppCompatActivity {
@@ -15,12 +16,21 @@ public class Register_Activity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
 
         ImageButton btnVolver = findViewById(R.id.btn_backr);
+        Button RResgister = findViewById(R.id.btn_registerM);
 
         btnVolver.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent log = new Intent(Register_Activity.this,Login_Activity.class);
-                startActivity(log);
+                Intent main = new Intent(Register_Activity.this,Main_Activity.class);
+                startActivity(main);
+            }
+        });
+
+        RResgister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent Log = new Intent(Register_Activity.this,Login_Activity.class);
+                startActivity(Log);
             }
         });
     }
---
+++ app/src/main/java/com/mflorezddelgado/t_mate/TeamViewCoach.java
@@ -0,0 +1,14 @@
+package com.mflorezddelgado.t_mate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TeamViewCoach extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_team_view_coach);
+    }
+}
