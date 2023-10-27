a186ade1ed386f94a6b94c302438a66051f988db
Carlos J <cabaunza@unab.edu.co>
2023-10-27 17:41:22 -0500

    Avance Carlos Abaunza Views TrainingDescriptionActivity, CreateTrainingActivity; agregados los colores a colors.xml y textos del footer a strings.xml y sus logos a la carpeta drawable

---
+++ app/src/main/java/com/mflorezddelgado/t_mate/TrainingDescriptionActivity.java
@@ -0,0 +1,14 @@
+package com.mflorezddelgado.t_mate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TrainingDescriptionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_training_description);
+    }
+}
