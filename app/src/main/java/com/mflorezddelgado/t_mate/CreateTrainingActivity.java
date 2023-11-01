45215ae0618429107e876e0623e4998878d442e2
Carlos J <cabaunza@unab.edu.co>
2023-11-01 17:30:38 -0500

    Avance Carlos Abaunza BD

--- app/src/main/java/com/mflorezddelgado/t_mate/CreateTrainingActivity.java
+++ app/src/main/java/com/mflorezddelgado/t_mate/CreateTrainingActivity.java
@@ -1,14 +1,50 @@
 package com.mflorezddelgado.t_mate;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.Firebase;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class CreateTrainingActivity extends AppCompatActivity {
+
+    private EditText etTitulo, etDescripcion, etCaracteristicas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_training);
+
+        etTitulo = findViewById(R.id.ed_text_titulo);
+        etDescripcion = findViewById(R.id.ed_text_descripcion);
+        etCaracteristicas = findViewById(R.id.ed_text_caracteristicas);
     }
+    public void clickGuardarEjercicio(View view){
+        Entrenamiento nuevoEntrenamiento = new Entrenamiento();
+        nuevoEntrenamiento.setTitulo(etTitulo.getText().toString());
+        nuevoEntrenamiento.setDescripcion(etDescripcion.getText().toString());
+        nuevoEntrenamiento.setCaracteristicas(etCaracteristicas.getText().toString());
+
+        FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+        firestore.collection("Entrenamiento").add(nuevoEntrenamiento).addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentReference> task) {
+                if (task.isSuccessful()){
+                    Toast.makeText(CreateTrainingActivity.this, "Se creó el registro", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(CreateTrainingActivity.this, task.getException().toString() , Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+
 }
---
+++ app/src/main/java/com/mflorezddelgado/t_mate/Entrenamiento.java
@@ -0,0 +1,42 @@
+package com.mflorezddelgado.t_mate;
+
+public class Entrenamiento {
+
+    private String titulo;
+    private String descripcion;
+    private String caracteristicas;
+
+    public Entrenamiento(String titulo, String descripcion, String caracteristicas) {
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+        this.caracteristicas = caracteristicas;
+    }
+
+    public Entrenamiento() {
+    }
+
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getCaracteristicas() {
+        return caracteristicas;
+    }
+
+    public void setCaracteristicas(String caracteristicas) {
+        this.caracteristicas = caracteristicas;
+    }
+}
