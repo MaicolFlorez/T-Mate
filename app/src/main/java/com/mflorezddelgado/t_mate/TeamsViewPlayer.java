5d4d6f207dad8335b72f5e32af8e21916b0550c2
Carlos J <cabaunza@unab.edu.co>
2023-11-03 16:59:46 -0500

    Aqui

--- app/src/main/java/com/mflorezddelgado/t_mate/TeamsViewPlayer.java
+++ app/src/main/java/com/mflorezddelgado/t_mate/TeamsViewPlayer.java
@@ -45,6 +45,10 @@ public class TeamsViewPlayer extends AppCompatActivity {
         elements.add(new ListElement("#775227", "Mogus", "Fuchibol", "Crewmate"));
         elements.add(new ListElement("#775447", "Sos", "Futbol", "Directivo"));
         elements.add(new ListElement("#775227", "Mogus", "Fuchibol", "Crewmate"));
+        elements.add(new ListElement("#775447", "Sos", "Futbol", "Directivo"));
+        elements.add(new ListElement("#775227", "Mogus", "Fuchibol", "Crewmate"));
+        elements.add(new ListElement("#775447", "Sos", "Futbol", "Directivo"));
+        elements.add(new ListElement("#775227", "Mogus", "Fuchibol", "Crewmate"));
 
         ListAdapter listAdapter = new ListAdapter(elements,this);
         RecyclerView recyclerView =findViewById(R.id.listTeamsView);
